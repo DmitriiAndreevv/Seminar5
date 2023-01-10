424754ed6909e27ecab2f74ec18f26d97e3eb772
DMITRII ANDREEV <111002352+DmitriiAndreevv@users.noreply.github.com>
2023-01-10 23:34:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Solution t1 = new Solution();
-        Solution2 t2 = new Solution2();
+        System.out.println("Hello world!");
+        Map<String,Integer> map = new HashMap<>();
+        String text = "sssddd asds asdds afggdf aa aa aa aa aa";
+        String[] words = text.split("");
+        for(String word: words) {
+            
+            map.putIfAbsent(word, 0);
+            map.put(word, map.get(word) + 1);
+        }
+        System.out.println(map);
     }
 }
